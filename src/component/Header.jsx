4b4c010a01f0db4c6961d359b1beece7ebcbad18
UWAJENEZANadine <adelineu640@gmail.com>
2023-03-08 00:18:55 +0200

    styles

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,23 +1,46 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "./img/logo.png"
-import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
-// import logo from "../component/img/logo.png";
-
+import logo from "./img/logo.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { Select, Option } from "@material-tailwind/react";
+import "./css/App.css"
 
 class Header extends Component {
   render() {
     return (
       <div className="navigation text-[#4c4cf7] bg-black flex justify-between items-center px-4 h-24 max-w-[1240px mx-auto]">
         <div>
-          <img src={logo} alt="logo"/>
+          <img src={logo} alt="logo" />
         </div>
-        <div className="flex">
-          <NavLink className="p-4" to='home'>Home</NavLink>
-          <NavLink className="p-4" to='services'>Country List</NavLink>
-          <NavLink className="p-4" to='about'>About us</NavLink>
-          <NavLink className="p-4" to='main'>Main</NavLink>
+        <input
+          className="bg-gray-300 rounded-md p-1 w-96"
+          type="text"
+          onChange={(e) => this.setState({ filterdata: e.target.value })}
+          placeholder="search here"
+        />
+        <div className="flex headerr">
+          <NavLink className="p-4" to="home">
+            Home
+          </NavLink>
+          <NavLink className="p-4" to="services">
+            Country List
+          </NavLink>
+          <NavLink className="p-4" to="about">
+            About us
+          </NavLink>
+          <NavLink className="p-4" to="main">
+            Main
+          </NavLink>
           {/* <NavLink to='contact'>contact</NavLink> */}
+        </div>
+        <div className="w-72 text-blue-500">
+          <Select label="Select by Country">
+            <Option>Africa</Option>
+            <Option>America</Option>
+            <Option>Asia</Option>
+            <Option>Europe</Option>
+            <Option>Oceania</Option>
+          </Select>
         </div>
       </div>
     );
